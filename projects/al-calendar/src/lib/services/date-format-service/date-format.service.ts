15e59f6f411e93e4770de2567db30b0d1096fb6e
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2021-06-27 14:06:20 +0300

    TimeAzone file has been added.

--- projects/al-calendar/src/lib/services/date-format-service/date-format.service.ts
+++ projects/al-calendar/src/lib/services/date-format-service/date-format.service.ts
@@ -8,8 +8,26 @@ import { EmptyDateError } from '../../errors/empty-date-error';
 export class DateFormatService {
   prefixStart = '01/01/';
   prefixEnd = '12/31/';
-  constructor() { }
 
+  DATE_FORMATS = {
+
+  }
+  private dateSeperator = '-'
+  constructor(dateSeperator?: string) {
+    this.setDateFormat(dateSeperator);
+  }
+
+  setDateFormat(dateSeperator: string) {
+    this.dateSeperator = dateSeperator || '-';
+    this.DATE_FORMATS = {
+      ddMMYYYY: `dd${this.dateSeperator}MM${this.dateSeperator}yyyy`,
+      MMddYYYY: `MM${this.dateSeperator}dd${this.dateSeperator}yyyy`,
+      YYYYddMM: `yyyy${this.dateSeperator}dd${this.dateSeperator}MM`,
+      yyyyMMdd: `yyyy${this.dateSeperator}MM${this.dateSeperator}dd`,
+      yyyyMM: `yyyy${this.dateSeperator}MM`,
+      MMyyyy: `MM${this.dateSeperator}yyyy`,
+    }
+  }
 
   formatMMDDYYYY(date: Date) {
     if (!date) {
@@ -37,4 +55,9 @@ export class DateFormatService {
   formatDateToString(date: Date) {
     return `${formatDate(date, 'yyyy-MM-dd', 'en-US')}T00:00:00`;
   }
+
+  stripUTC(date: any): string {
+    return `${date}`.replace('Z', '');
+  }
+  // 2016-01-01T00:00:00 2016-01-01T00:00:00Z   Remove Z when user wants to
 }
