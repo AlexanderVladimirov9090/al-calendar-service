ffc41c9ad3c4e5a97e95d1683524e2d9bc38b07a
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2022-01-20 14:48:48 +0200

    Export.

--- projects/al-calendar/src/lib/al-calendar.module.ts
+++ projects/al-calendar/src/lib/al-calendar.module.ts
@@ -1,5 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CalendarService } from './services/calendar-service/calendar.service';
+import { DateFormatService } from './services/date-format-service/date-format.service';
+import { ValidationService } from './validation/validation.service';
 
 
-@NgModule({})
+@NgModule({
+  providers: [
+    CalendarService,
+    DateFormatService,
+    ValidationService
+  ]
+})
 export class AlCalendarModule { }
+export { CalendarService } from './services/calendar-service/calendar.service';
+export { DateFormatService } from './services/date-format-service/date-format.service';
+export { ValidationService } from './validation/validation.service';
+
